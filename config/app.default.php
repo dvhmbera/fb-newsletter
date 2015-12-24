a84d1f09e5484cb71c954137aebc1fa68e18ef48
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2015-12-24 21:31:34 +0200

    Remove composer.json, php app files

--- config/app.default.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Configuration for the application
-return [
-    'debug' => true,
-    'fb' => [
-        'app_id' => env('FACEBOOK_APP_ID'),
-        'app_secret' => env('FACEBOOK_APP_SECRET'),
-    ],
-];
